afb8edd18c6e69fefee14007ffdef8c28cbe83f6
MUHAMMADAZAN1 <144342317+MUHAMMADAZAN1@users.noreply.github.com>
2023-10-22 20:24:59 +0500

    when user uded zero

--- dataplan.c
+++ dataplan.c
@@ -2,7 +2,7 @@
 int main(){
     // declare variable
     float monthlyPackage,usedPackage,averagePackageUsedDaily,dailyUsedPackage,remainingPackageUsedDaily,packageExceeding,remainingPackage;
-    int days,remainingDays,
+    int days,remainingDays;
  //prompt msg to user
      printf("Enter your monthly package:");
     scanf("%f",&monthlyPackage);
@@ -23,3 +23,11 @@ int main(){
      printf("You are EXCEEDING your average daily use ( %f GB/day)\n",exceedDailyUsed);
      printf("Continuing this high usage, you'll exceed your data plan by %f GB\n",packageExceeding);
      printf("To stay below your data plan, use no more than %f GB/day\n", remainingPackage);
+      } else if(usedPackage==0){
+     averagePackageUsedDaily=monthlyPackage/30;
+     printf("_____________________________________________\n");
+     printf("%i days used, %i days remaining\n",days,remainingDays);
+     printf("Average daily use: %f GB/day\n",dailyUsedPackage);
+     printf("You are at or below your average daily use (%f GB/day)\n",averagePackageUsedDaily);
+     printf("You can use up to %f GB/day and stay below your data plan limit\n",remainingPackageUsedDaily);
+     }
