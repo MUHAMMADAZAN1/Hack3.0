6c2a49438908f541f7166656a9d6be837b0546d3
MUHAMMADAZAN1 <144342317+MUHAMMADAZAN1@users.noreply.github.com>
2023-10-22 20:07:56 +0500

    variables decleared as per requirements

---
+++ dataplan.c
@@ -0,0 +1,5 @@
+#include<stdio.h>
+int main(){
+    // declare variable
+    float monthlyPackage,usedPackage,averagePackageUsedDaily,dailyUsedPackage,remainingPackageUsedDaily,packageExceeding,remainingPackage;
+    int days,remainingDays,
