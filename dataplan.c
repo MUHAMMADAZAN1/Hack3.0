43bc89319de6fa066f492da04141b8884df43974
MUHAMMADAZAN1 <144342317+MUHAMMADAZAN1@users.noreply.github.com>
2023-10-22 20:12:15 +0500

    from input find remainings

--- dataplan.c
+++ dataplan.c
@@ -3,3 +3,13 @@ int main(){
     // declare variable
     float monthlyPackage,usedPackage,averagePackageUsedDaily,dailyUsedPackage,remainingPackageUsedDaily,packageExceeding,remainingPackage;
     int days,remainingDays,
+ //prompt msg to user
+     printf("Enter your monthly package:");
+    scanf("%f",&monthlyPackage);
+    printf("How many days you used Package:");
+    scanf("%i",&days);
+    printf("How much Gb of your Package you used:");
+    scanf("%f",&usedPackage);
+     remainingDays=30-days;
+    remainingPackage=monthlyPackage-usedPackage;
+    dailyUsedPackage=usedPackage/days;
